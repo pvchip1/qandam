a34106bbfcdf8604a08db02b5b81f7626609e797
Paulo Vinicius <pvchip1@gmail.com>
2021-03-30 12:31:39 -0300

    Add edit Admin template

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -3,4 +3,9 @@ class AdminsBackoffice::AdminsController <  AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+
   namespace :admins_backoffice do
-    get 'admins/index'
+       get 'welcome/index'
+       resources :admins, only: %w[ index edit ]
+    # get 'admins/index'
+    # get 'admins/edit/:id', to: 'admins#edit'
   end
 	namespace :site do
 			get 'welcome/index'
 	end
 	namespace :users_backoffice do
-			get 'welcome/index'
-	end
-	namespace :admins_backoffice do
 			get 'welcome/index'
 	end
 	
@@ -18,5 +19,6 @@ Rails.application.routes.draw do
 	get 'start', to: 'site/welcome#index'
 
 	root 'site/welcome#index'
+
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
