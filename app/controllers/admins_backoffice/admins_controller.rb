ce1c0a0f9519f1529f19c96ae118ea94220d654d
Paulo Vinicius <pvchip1@gmail.com>
2021-04-29 11:19:04 -0300

    Create action destroy, partions form

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,25 +1,34 @@
 class AdminsBackoffice::AdminsController <  AdminsBackofficeController
+  before_action :set_id, only: %i[edit update destroy ]
+  before_action :check_password_blank, only: [:update]
 
   def index
     @admins = Admin.all
   end
+  
+  def new
+    @admin = Admin.new
+  end
+  
+  def create
+    
+    @admin = Admin.new(set_params_admin)
+
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: 'Created Sucess!'
+    else
+      render :index
+    end
+  end
+  
 
   def edit
-    @admin = Admin.find(params[:id])
   end
   
   def update
-
-    if params[:password].blank? && params[:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin = Admin.find(params[:id])
     #require key "admin" comming from form
-     params_admin = params.require(:admin).permit( :email, :password, :password_confirmation )
-
-    if @admin.update(params_admin)
-      redirect_to edit_admins_backoffice_admin_path, notice: 'Sucess!'
+    if @admin.update(set_params_admin)
+      redirect_to edit_admins_backoffice_admin_path, notice: 'Updated Sucess!'
     else
       render :edit
     end
@@ -27,4 +36,30 @@ class AdminsBackoffice::AdminsController <  AdminsBackofficeController
     #   puts admin.errors.full_messages
     # end
   end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: 'Deleted Sucess!'
+    else
+      render :index
+    end
+  end
+  
+
+  private
+  
+  def check_password_blank
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+  
+  def set_id
+    @admin = Admin.find(params[:id])
+  end
+  
+  def set_params_admin
+    params_admin = params.require(:admin).permit( :email, :password, :password_confirmation )
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
        get 'welcome/index'
-       resources :admins, only: %i[ index edit update ]
+       #resources :admins, only: %i[ index edit update new create]
+       resources :admins#, except: :destroy
     # get 'admins/index'
     # get 'admins/edit/:id', to: 'admins#edit'
   end
